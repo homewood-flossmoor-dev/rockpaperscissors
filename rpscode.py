291118cd9137c4960e4d8f54695b50011864272b
Green-TS <117697317+Green-TS@users.noreply.github.com>
2023-01-27 11:31:57 -0600

    updated code

--- rpscode.py
+++ rpscode.py
@@ -13,3 +13,21 @@ while player == False:
     elif player == "Rock":
         if computer == "Paper":
             print("You Lose!", computer, "covers", player)
+                else:
+            print("You win!", player, "smashes", computer)
+    elif player == "Paper":
+        if computer == "Scissors":
+            print("You lose!", computer, "cut", player)
+        else:
+            print("You win!", player, "covers", computer)
+    elif player == "Scissors":
+        if computer == "Rock":
+            print("You lose...", computer, "smashes", player)
+        else:
+            print("You win!", player, "cut", computer)
+    else:
+        print("That's not a valid play. Check your spelling!")
+        
+player = False
+
+computer = t[randint(0,2)]
